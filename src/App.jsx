9c6e15f2158e4bfae22ed52b828497f9089aa5dc
Matthew Banh <mattbanh92@gmail.com>
2023-01-21 23:15:15 -0800

    separated form and entries components and added delete entry functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,20 @@
 import { useState } from "react";
 
 import vcdLogo from "./assets/vcd-logo.png";
+import { CountupEntries } from "./components/CountupEntries/CountupEntries";
+import { CountupForm } from "./components/CountupForm/CountupForm";
 
 function App() {
-  const [values, setValues] = useState({ val: [] });
-
-  function createInputs() {
-    return values.val.map((el, i) => (
-      <div className="flex gap-3 mb-4" key={i}>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-40 text-sm bg-slate-400 text-slate-50 placeholder-slate-300"
-            type="input"
-            placeholder="Name"
-            value={el || ""}
-            onChange={(event) => handleChangeName(event, i)}
-          ></input>
-        </label>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-20 text-sm bg-slate-400 text-slate-50 placeholder-slate-300"
-            type="input"
-            placeholder="Score"
-            value={el || ""}
-            onChange={(event) => handleChangeScore(event, i)}
-          ></input>
-        </label>
-        <input
-          className="text-xs"
-          type="button"
-          value="remove"
-          name={i}
-          onClick={(event) => removeClick(event, i)}
-        />
-      </div>
-    ));
-  }
-
-  function handleChangeName(event, i) {
-    let vals = [...values.val];
-    vals[i] = event.target.value;
-    setValues({ val: vals });
-  }
-
-  function handleChangeScore(event, i) {
-    let vals = [...values.val];
-    vals[i] = event.target.value;
-    setValues({ val: vals });
-  }
-
-  const addClick = () => {
-    setValues({ val: [...values.val, ""] });
-  };
-
-  const removeClick = (event) => {
-    let vals = [...values.val];
-    let index = Number(event.target.name);
-    vals.splice(index, 1);
-    setValues({ val: vals });
-  };
-
-  const handleSubmit = (event) => {
-    alert("A name was submitted: " + values.val.join(", "));
-    event.preventDefault();
-  };
+  const [newEntry, setNewEntry] = useState([]);
 
   return (
     <section className="m-4">
       <div className="mb-10">
         <img className="w-48 mx-auto" src={vcdLogo}></img>
       </div>
-      <h1 className="mb-4">Countup</h1>
-      <form onSubmit={handleSubmit}>
-        {createInputs()}
-        <div className="flex gap-4">
-          <button
-            type="button"
-            className="text-sm bg-slate-600 rounded-sm py-1 px-2"
-            onClick={addClick}
-          >
-            Add
-          </button>
-          <button
-            type="submit"
-            className="text-sm bg-slate-600 rounded-sm py-1 px-2"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      {/* <form onSubmit={handleSubmit}>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-40 text-sm"
-            type="input"
-            placeholder="Name"
-          ></input>
-        </label>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-20 text-sm"
-            type="input"
-            placeholder="Score"
-          ></input>
-        </label>
-        <button
-          className="text-sm bg-slate-600 rounded-sm py-1 px-2"
-          onClick={addClick}
-        >
-          Add
-        </button>
-      </form> */}
+
+      <CountupEntries newEntry={newEntry} />
+      <CountupForm setNewEntry={setNewEntry} />
     </section>
   );
 }
--- src/components/CountupForm/CountupForm.jsx
+++ src/components/CountupForm/CountupForm.jsx
@@ -1,96 +1,69 @@
-export function CountupForm() {
-  //   const [values, setValues] = useState({val: []});
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-  //   function createInputs() {
-  //     return values.val.map((el, i) => (
-  //       <div key={i}>
-  //         <input type="text" value={el || ''} onChange={handleChange.bind(i)} />
-  //         <input
-  //           type="button"
-  //           value="remove"
-  //           name={i}
-  //           onClick={removeClick.bind(i)}
-  //         />
-  //       </div>
-  //     ));
-  //   }
+const URL = import.meta.env.VITE_API_BACKEND_URL;
+const PORT = import.meta.env.VITE_API_PORT;
 
-  //   function handleChange(event) {
-  //     let vals = [...values.val];
-  //     vals[this] = event.target.value;
-  //     setValues({val: vals});
-  //   }
+const initialValues = { name: "", score: "" };
 
-  //   const addClick = () => {
-  //     setValues({val: [...values.val, '']});
-  //   };
+export function CountupForm({ setNewEntry }) {
+  const [values, setValues] = useState(initialValues);
 
-  //   const removeClick = (event) => {
-  //     let vals = [...values.val];
-  //     let index = Number(event.target.name);
-  //     vals.splice(index, 1);
-  //     setValues({val: vals});
-  //   };
-  async function sendFormData(event) {
-    // Preparing API request using fetch
-    let formDataObject = {
-      name: event.target.name.value,
-      score: event.target.score.value,
-    };
-    let formDataJsonString = JSON.stringify(formDataObject);
-    let fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: formDataJsonString,
-    };
+  const addEntry = (newEntry) => {
+    const { name, score } = newEntry;
 
-    // API written in routes -> api -> addProduct.server.js
-    let res = await fetch(`/api/countupDB`, fetchOptions);
-    if (!res.ok) {
-      let error = await res.text();
-      throw new Error(error);
-    } else {
-      return "Success";
-    }
-  }
+    axios
+      .post(`${URL}${PORT}/entries`, { name, score: parseInt(score) })
+      .then((response) => {
+        console.log(response);
+        setNewEntry(response.data);
+        setValues(initialValues);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
 
   const handleSubmit = (event) => {
-    sendFormData(event);
+    event.preventDefault();
+    addEntry(values);
   };
 
   return (
-    <section className="m-4">
-      <div className="mb-10">
-        {/* <img className="w-48 mx-auto" src={vcdLogo}></img> */}
-      </div>
-      <h1 className="mb-4">Countup</h1>
-      {/* <form className="flex gap-3 mb-4" onSubmit={handleSubmit}> */}
-      <form className="flex gap-3 mb-4" onSubmit={handleSubmit}>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-40 text-sm"
-            type="input"
-            name="name"
-            placeholder="Name"
-          ></input>
-        </label>
-        <label>
-          <input
-            className="rounded-sm px-2 py-1 w-20 text-sm"
-            type="input"
-            name="score"
-            placeholder="Score"
-          ></input>
-        </label>
-        <button
-          className="text-sm bg-slate-600 rounded-sm py-1 px-2"
-          //   onClick={addClick}
-        >
-          Add
-        </button>
+    <section className="md:m-4">
+      <h2 className="font-bold mb-4">Add Entries</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="flex gap-2 mb-4">
+          <label>
+            <input
+              className="rounded-sm px-2 py-1 w-40 text-sm bg-slate-400 text-slate-50 placeholder-slate-300"
+              type="input"
+              placeholder="Name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label>
+            <input
+              className="rounded-sm px-2 py-1 w-20 text-sm bg-slate-400 text-slate-50 placeholder-slate-300"
+              type="number"
+              name="score"
+              placeholder="Score"
+              value={values.score}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <button
+            type="submit"
+            className="text-xs bg-slate-600 rounded-sm py-1 px-2"
+          >
+            Add
+          </button>
+        </div>
       </form>
     </section>
   );
